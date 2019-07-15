50aaa186e3ed6e767a7151a6da575f2faed06bc3
Seyi <seyipaye@gmail.com>
2019-07-15 01:32:26 +0100

    Latest Commit with UI friendly codes

--- app/src/main/java/ng/com/alc4phase1/AboutActivity.java
+++ app/src/main/java/ng/com/alc4phase1/AboutActivity.java
@@ -1,35 +1,78 @@
 package ng.com.alc4phase1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class AboutActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        WebView webView= findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
 
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new SSLTolerentWebViewClient());
+        webView.setWebViewClient(new CustomWebViewClient());
         webView.loadUrl("https://andela.com/alc/");
-        //webView.getProgress()
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    // SSL Error Tolerant Web View Client
-    public class SSLTolerentWebViewClient extends WebViewClient {
+    // Custom Web View Client
+    public class CustomWebViewClient extends WebViewClient {
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
             handler.proceed(); // Ignore SSL certificate errors
         }
 
+        @Override
+        public void onPageCommitVisible(WebView view, String url) {
+            super.onPageCommitVisible(view, url);
+            progressBar.setVisibility(View.INVISIBLE);
+            webView.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            Log.e("test", "Error: " + error);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Toast.makeText(AboutActivity.this, "Oh no! " + error.getDescription().toString() , Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(AboutActivity.this, "Sorry there's an error", Toast.LENGTH_SHORT).show();
+            }
+            super.onReceivedError(view, request, error);
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/ng/com/alc4phase1/ProfileActivity.java
+++ app/src/main/java/ng/com/alc4phase1/ProfileActivity.java
@@ -1,11 +1,15 @@
 package ng.com.alc4phase1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,6 +23,16 @@ public class ProfileActivity extends AppCompatActivity {
                 .load(R.drawable.my_pix)
                 .into(myImage);
 
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
